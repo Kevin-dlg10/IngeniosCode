6d61264c4c66d43d297096d1219756a36990e0d2
Kevin-dlg10 <63732514+Kevin-dlg10@users.noreply.github.com>
2020-04-16 20:31:48 -0500

    showMenu() done

--- .github/workflows/HW3.cpp
+++ .github/workflows/HW3.cpp
@@ -1,3 +1,44 @@
 - name: Setup C++
   uses: Trass3r/setup-cpp@v1
 // Samantha start code from here 
+#include <iostream>
+using namespace std;
+void showMenu();
+int main()
+{
+    showMenu();
+
+
+
+
+    return 0;
+}
+void showMenu()
+{
+    char op;
+    cout << "Rational Numbers Calculator" << endl;
+    cout << endl;
+    cout <<"(A) ddition" <<endl;
+    cout <<"(S) ubtraction" << endl;
+    cout <<"(Q) uit" << endl;
+    cout << endl;
+    cout <<"Enter your option: ";
+    cin >> op;
+    if( op == 'A' || op == 'a')
+    {
+        add();
+    }
+    else if(op == 'S' || op == 's')
+    {
+        sub();
+    }
+    else if(op = 'Q'|| op = 'q')
+    {
+        cout << "thank you!" << endl;
+    }
+    else
+    {
+        cout << "please enter options: A ,S ,or Q"
+    }
+    
+}
