083a2e34cc18440ce0b83cea21caa65082c818be
Kevin-dlg10 <63732514+Kevin-dlg10@users.noreply.github.com>
2020-04-18 14:35:35 -0500

    Complete Update
    i finished the program, please check getRational, im having second thoughts on that and would prefer a different approach

--- .github/workflows/HW3.cpp
+++ .github/workflows/HW3.cpp
@@ -1,6 +1,15 @@
 - name: Setup C++
   uses: Trass3r/setup-cpp@v1
 // Samantha start code from here 
+//******************************************************************************
+// Team #3 CSCI/CMPE 1370 Spring2020 Homework # 3
+// Kevin De La Garza 
+// Samantha Garza
+// This program is a calculator that can add and subtract rational numbers.
+// the user can implement any kind of fractions, and our algorythm will get it done efficiently
+// through the use of functions and string values with applying all the basics we have learned in class.
+//including eliuds algorythm for reduction.
+//******************************************************************************
 #include <iostream>
 #include <stdlib.h>
 #include <string>
@@ -9,8 +18,6 @@ using namespace std;
 void showMenu();
 void add();
 void sub();
-//void addRational();
-
 int main()
 {
     char op;
@@ -56,12 +63,50 @@ void getRational(int *num, int *den)
     getline (s_stream1,denstr,'/');
     *num = stoi(numstr);
     *den = stoi(denstr);
-    cout << *num << *den << endl;
     
+}
+void addRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
+{
+    *anum = (num1 * den2) + (num2 * den1);
+    *aden = (den1 * den2);
+}
+void subRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
+{
+    *anum = (num1 * den2) - (num2 * den1);
+    *aden = (den1 * den2);
+}
+void reduced(int *num,int *den)
+{
+    int anum, aden;
+    int R = anum % aden;
+    while( R != 0)
+    {
+        anum = aden;
+        aden = R;
+        R = anum % aden;
+    }
+    int gcf = aden;
+    *num = anum / gcf;
+    *den = aden / gcf;
+}
+void displayRational (int num, int den)
+{
+    char dummy = '/';
+    if(den = 1)
+    {
+        cout << "the result is: " << num << endl; 
+    }
+    else
+    {
+        cout << "the result is: " << num << dummy << den <<endl;
+    }
+    
+
 }
 void add()
 {
     system("CLS");
+    int num,den;
     int num1,den1,num2,den2,anum,aden;
     while(true)
     {
@@ -70,11 +115,23 @@ void add()
         cout << endl;
         getRational(&num1, &den1);
         getRational(&num2, &den2);
+        addRational(&anum,&aden, num1, den1,num2,den2);
+        reduced(&anum,&aden);
+        displayRational(num,den);
+        char user = '\0';
+        cout << "Do you want to do more additions? (Y/N): ";
+        cin >> user;
+        cin.ignore();
+        if(user == ('N' || 'n'))
+        {
+            break;
+        }
     }
 }
 void sub()
 {
     system("CLS");
+    int num,den;
     int num1,den1,num2,den2,anum,aden;
     while(true)
     {
@@ -82,5 +139,16 @@ void sub()
         cout << endl;
         getRational(&num1, &den1);
         getRational(&num2, &den2);
+        addRational(&anum,&aden, num1, den1,num2,den2);
+        reduced(&anum,&den);
+        displayRational(num,den);
+        char user = '\0';
+        cout << "Do you want to do more additions? (Y/N): ";
+        cin >> user;
+        cin.ignore();
+        if(user == ('N' || 'n'))
+        {
+            break;
+        }
     }
 }
