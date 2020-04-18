19dd878adcfa4e6fa2538616455084ae3f0d58b5
Kevin-dlg10 <63732514+Kevin-dlg10@users.noreply.github.com>
2020-04-17 20:19:38 -0500

    changes to program 4/17/2020
    added more to the program.

--- .github/workflows/HW3.cpp
+++ .github/workflows/HW3.cpp
@@ -2,26 +2,34 @@
   uses: Trass3r/setup-cpp@v1
 // Samantha start code from here 
 #include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
 using namespace std;
 void showMenu();
+void add();
+void sub();
+//void addRational();
+
 int main()
 {
+    char op;
     showMenu();
-    if( op == 'A' || op == 'a')
+    if( op = ('A' || 'a'))
     {
         add();
     }
-    else if(op == 'S' || op == 's')
+    else if(op = ('S' || 's'))
     {
         sub();
     }
-    else if(op = 'Q'|| op = 'q')
+    else if(op = ('Q'||'q'))
     {
         cout << "thank you!" << endl;
     }
     else
     {
-        cout << "please enter options: A ,S ,or Q"
+        cout << "Please enter options: A ,S ,or Q"<< endl;
     }
 
     return 0;
@@ -38,5 +46,41 @@ void showMenu()
     cout <<"Enter your option: ";
     cin >> op;
 }
-
-
+void getRational(int *num, int *den)
+{
+    string numstr,denstr,str;
+    cout << "Please Enter a fraction (n/d): ";
+    getline(cin,str);
+    stringstream s_stream1(str);
+    getline (s_stream1,numstr,'/');
+    getline (s_stream1,denstr,'/');
+    *num = stoi(numstr);
+    *den = stoi(denstr);
+    cout << *num << *den << endl;
+    
+}
+void add()
+{
+    system("CLS");
+    int num1,den1,num2,den2,anum,aden;
+    while(true)
+    {
+        cout << "Addition of Rational Numbers" << endl;
+        
+        cout << endl;
+        getRational(&num1, &den1);
+        getRational(&num2, &den2);
+    }
+}
+void sub()
+{
+    system("CLS");
+    int num1,den1,num2,den2,anum,aden;
+    while(true)
+    {
+        cout << "Subtraction of Rational Numbers" << endl;
+        cout << endl;
+        getRational(&num1, &den1);
+        getRational(&num2, &den2);
+    }
+}
