88e2d5e872cac9223e54dfc46c345eeea89bea66
Kevin-dlg10 <63732514+Kevin-dlg10@users.noreply.github.com>
2020-04-18 19:56:33 -0500

    comppleteish xD

--- .github/workflows/HW3.cpp
+++ .github/workflows/HW3.cpp
@@ -15,65 +15,32 @@
 #include <string>
 #include <sstream>
 using namespace std;
-void showMenu();
-void add();
-void sub();
-int main()
-{
-    char op;
-    showMenu();
-    if( op = ('A' || 'a'))
-    {
-        add();
-    }
-    else if(op = ('S' || 's'))
-    {
-        sub();
-    }
-    else if(op = ('Q'||'q'))
-    {
-        cout << "thank you!" << endl;
-    }
-    else
-    {
-        cout << "Please enter options: A ,S ,or Q"<< endl;
-    }
-
-    return 0;
-}
 void showMenu()
 {
-    char op;
     cout << "Rational Numbers Calculator" << endl;
     cout << endl;
     cout <<"(A) ddition" <<endl;
     cout <<"(S) ubtraction" << endl;
     cout <<"(Q) uit" << endl;
     cout << endl;
-    cout <<"Enter your option: ";
-    cin >> op;
 }
-void getRational(int *num, int *den)
+void displayRational (int num, int den)
 {
-    string numstr,denstr,str;
-    cout << "Please Enter a fraction (n/d): ";
-    getline(cin,str);
-    stringstream s_stream1(str);
-    getline (s_stream1,numstr,'/');
-    getline (s_stream1,denstr,'/');
-    *num = stoi(numstr);
-    *den = stoi(denstr);
+    char dummy = '/';
+    if(den = 1)
+    {
+        cout << "the result is: " << num << endl; 
+    }
+    else if (num = 0)
+    {
+        cout << "The result is 0"<< endl;
+    }
+    else
+    {
+        cout << "the result is: " << num << dummy << den <<endl;
+    }
     
-}
-void addRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
-{
-    *anum = (num1 * den2) + (num2 * den1);
-    *aden = (den1 * den2);
-}
-void subRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
-{
-    *anum = (num1 * den2) - (num2 * den1);
-    *aden = (den1 * den2);
+
 }
 void reduced(int *num,int *den)
 {
@@ -89,29 +56,37 @@ void reduced(int *num,int *den)
     *num = anum / gcf;
     *den = aden / gcf;
 }
-void displayRational (int num, int den)
+void addRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
 {
-    char dummy = '/';
-    if(den = 1)
-    {
-        cout << "the result is: " << num << endl; 
-    }
-    else
-    {
-        cout << "the result is: " << num << dummy << den <<endl;
-    }
+    *anum = (num1 * den2) + (num2 * den1);
+    *aden = (den1 * den2);
+}
+void subRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
+{
+    *anum = (num1 * den2) - (num2 * den1);
+    *aden = (den1 * den2);
+}
+void getRational(int *num, int *den)
+{
+    string str;
+    string numstr,denstr;
+    cout << "Please Enter a fraction (n/d): "; 
+    getline(cin,str);
+    stringstream str_stream(str);
+    getline(str_stream,numstr,'/');
+    getline(str_stream,denstr,'/');
+   *num = stoi(numstr);
+    *den = stoi(denstr);
     
-
 }
 void add()
 {
     system("CLS");
     int num,den;
     int num1,den1,num2,den2,anum,aden;
+     cout << "Addition of Rational Numbers" << endl;
     while(true)
     {
-        cout << "Addition of Rational Numbers" << endl;
-        
         cout << endl;
         getRational(&num1, &den1);
         getRational(&num2, &den2);
@@ -126,6 +101,10 @@ void add()
         {
             break;
         }
+        else 
+        {
+            showMenu();
+        }
     }
 }
 void sub()
@@ -133,22 +112,54 @@ void sub()
     system("CLS");
     int num,den;
     int num1,den1,num2,den2,anum,aden;
+    cout << "Subtraction of Rational Numbers" << endl;
     while(true)
     {
-        cout << "Subtraction of Rational Numbers" << endl;
-        cout << endl;
         getRational(&num1, &den1);
         getRational(&num2, &den2);
-        addRational(&anum,&aden, num1, den1,num2,den2);
+        subRational(&anum,&aden, num1, den1,num2,den2);
         reduced(&anum,&den);
         displayRational(num,den);
         char user = '\0';
         cout << "Do you want to do more additions? (Y/N): ";
         cin >> user;
         cin.ignore();
-        if(user == ('N' || 'n'))
+        if(user = 'N')
         {
             break;
         }
+        else
+        {
+            showMenu();
+        }
+        
     }
+}
+int main()
+{
+    char op;
+    int num,den;
+    int num1,den1,num2,den2,anum,aden;
+    showMenu();
+    cout <<"Enter your option: ";
+    cin >> op;
+    if(op == 'S')
+    {
+        sub();
+    }
+    else if (op == 'A')
+    {
+        add();
+    }
+    else if(op =='Q')
+    {
+        system("CLS");
+        cout << "thank you!" << endl;
+        
+    }
+    else
+    {
+        cout << "Please enter options: A ,S ,or Q"<< endl;
+    }
+    return 0;
 }
