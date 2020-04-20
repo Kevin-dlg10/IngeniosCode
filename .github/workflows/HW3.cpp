2dd55f544f69e10b907907aea81c2ed5045dc685
Kevin-dlg10 <63732514+Kevin-dlg10@users.noreply.github.com>
2020-04-19 21:35:52 -0500

    Complete!
    Samantha did a pre program for me to check, made some adjustments, and compared it to what my program can contribute to hers. thus, having this Program right now

--- .github/workflows/HW3.cpp
+++ .github/workflows/HW3.cpp
@@ -15,125 +15,96 @@
 #include <string>
 #include <sstream>
 using namespace std;
-void showMenu()
-{
+void addition();
+void subtraction();
+void disRat(int,int );
+
+//this function shows the menu, where everything begins.
+// we decided we didnt want to input here but to be used as a frame in some sort to just display the option
+void showMenu(){
     cout << "Rational Numbers Calculator" << endl;
-    cout << endl;
-    cout <<"(A) ddition" <<endl;
-    cout <<"(S) ubtraction" << endl;
-    cout <<"(Q) uit" << endl;
-    cout << endl;
+    cout << "(A)dditon" << endl;
+    cout << "(S)ubtraction" << endl;
+    cout << "(Q)uit" << endl;
+    cout << "Enter your option: " << endl;
 }
-void displayRational (int num, int den)
-{
-    char dummy = '/';
-    if(den = 1)
-    {
-        cout << "the result is: " << num << endl; 
+
+//this functions gets the rational of both fractions
+void getRat(int *num1, int *den1, int *num2,int *den2){
+    bool start = true;
+    string input1, input2;
+    string numstr1,denstr1;
+    string numstr2,denstr2;
+
+    while(start){
+        cout << "Please enter a function (n/d): "; 
+        cin >>input1;
+        stringstream str_stream(input1);
+        getline(str_stream,numstr1,'/');
+        getline(str_stream,denstr1,'/');
+        *num1 = stoi(numstr1);
+        *den1 = stoi(denstr1);
+        cout << "Please enter a function (n/d): "; 
+        cin >>input2;
+        stringstream inp_stream(input2);
+        getline(inp_stream,numstr2,'/');
+        getline(inp_stream,denstr2,'/');
+        *num2 = stoi(numstr2);
+        *den2 = stoi(denstr2);
+        if (den1==0 || den2 == 0){
+            cout << "Try again" << endl;
+            start=true;
+        }
+        else if(den1!=0 || den2 !=0){
+            start=false;
+        }
     }
-    else if (num = 0)
-    {
-        cout << "The result is 0"<< endl;
+}
+
+
+// finding  return value : GCF
+int reduce(int anum,int aden)
+{
+   if (aden == 0) 
+      return anum; 
+   return reduce(aden, anum % aden);  
+} 
+// we then applied reduce in the function of add
+// we then got the reduction in terms of our own, not in eliud's format
+void addRat(int anum,int aden, int num1, int den1,int num2,int den2)
+{
+    anum = ((num1*den2)+(num2*den1));
+    aden = (den1*den2);
+     int gcf = reduce(anum,aden); 
+     anum = anum / gcf;
+     aden = aden / gcf;
+    disRat(anum,aden);
+
+}
+void subRat(int anum, int aden, int num1, int den1, int num2, int den2)
+{
+    anum = ((num1*den2)-(num2*den1));
+    aden = (den1*den2);
+    int gcf = reduce(anum,aden); 
+    anum = anum / gcf;
+    aden = aden / gcf;
+    disRat(anum,aden);
+}
+
+void disRat(int anum, int aden)
+{
+    if (aden  > 1){
+        cout << "The result is: "<< anum << "/"<< aden << endl;
     }
     else
     {
-        cout << "the result is: " << num << dummy << den <<endl;
+        cout << "The result is: " << anum << endl;
     }
     
-
-}
-void reduced(int *num,int *den)
-{
-    int anum, aden;
-    int R = anum % aden;
-    while( R != 0)
-    {
-        anum = aden;
-        aden = R;
-        R = anum % aden;
-    }
-    int gcf = aden;
-    *num = anum / gcf;
-    *den = aden / gcf;
-}
-void addRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
-{
-    *anum = (num1 * den2) + (num2 * den1);
-    *aden = (den1 * den2);
-}
-void subRational(int *anum, int *aden, int num1, int den1, int num2, int den2)
-{
-    *anum = (num1 * den2) - (num2 * den1);
-    *aden = (den1 * den2);
-}
-void getRational(int *num, int *den)
-{
-    string str;
-    string numstr,denstr;
-    cout << "Please Enter a fraction (n/d): "; 
-    getline(cin,str);
-    stringstream str_stream(str);
-    getline(str_stream,numstr,'/');
-    getline(str_stream,denstr,'/');
-   *num = stoi(numstr);
-    *den = stoi(denstr);
-    
-}
-void add()
-{
+}   
+void quit(){
     system("CLS");
-    int num,den;
-    int num1,den1,num2,den2,anum,aden;
-     cout << "Addition of Rational Numbers" << endl;
-    while(true)
-    {
-        cout << endl;
-        getRational(&num1, &den1);
-        getRational(&num2, &den2);
-        addRational(&anum,&aden, num1, den1,num2,den2);
-        reduced(&anum,&aden);
-        displayRational(num,den);
-        char user = '\0';
-        cout << "Do you want to do more additions? (Y/N): ";
-        cin >> user;
-        cin.ignore();
-        if(user == ('N' || 'n'))
-        {
-            break;
-        }
-        else 
-        {
-            showMenu();
-        }
-    }
-}
-void sub()
-{
-    system("CLS");
-    int num,den;
-    int num1,den1,num2,den2,anum,aden;
-    cout << "Subtraction of Rational Numbers" << endl;
-    while(true)
-    {
-        getRational(&num1, &den1);
-        getRational(&num2, &den2);
-        subRational(&anum,&aden, num1, den1,num2,den2);
-        reduced(&anum,&den);
-        displayRational(num,den);
-        char user = '\0';
-        cout << "Do you want to do more additions? (Y/N): ";
-        cin >> user;
-        cin.ignore();
-        if(user = 'N')
-        {
-            break;
-        }
-        else
-        {
-            showMenu();
-        }
-        
-    }
+    cout << "Thank you bye bye!!" << endl;
 }
 int main()
 {
@@ -141,15 +112,14 @@ int main()
     int num,den;
     int num1,den1,num2,den2,anum,aden;
     showMenu();
-    cout <<"Enter your option: ";
     cin >> op;
     if(op == 'S')
     {
-        sub();
+        subtraction();
     }
     else if (op == 'A')
     {
-        add();
+        addition();
     }
     else if(op =='Q')
     {
@@ -162,4 +132,59 @@ int main()
         cout << "Please enter options: A ,S ,or Q"<< endl;
     }
     return 0;
+}
+void addition()
+{
+    //you can use Dr. Kim's reference code as a starting point to
+    //in his rough code, he uses 6 int variables
+   system("CLS");
+    int num1,den1,num2,den2,anum,aden;
+     cout << "Addition of Rational Numbers" << endl;
+    while(true)
+    {
+        cout << endl;
+        getRat(&num1, &den1, &num2, &den2); 
+        addRat(anum,aden, num1, den1,num2,den2); 
+        char user = '\0';
+        cout << "Do you want to do more additions? (Y/N): ";
+        cin >> user;
+        cin.ignore();
+        if(user == 'N')
+        {
+            quit();
+            break;
+        }
+        else 
+        {
+            addition();
+        }
+    }
+}
+
+//your subtraction and addition functions should look fairly similar since they do
+//basically the same job
+void subtraction()
+{
+//you need to use the same number of integer variables as the addition function
+    system("CLS");
+    int num1,den1,num2,den2,anum,aden;
+    cout << "Subtraction of Rational Numbers" << endl;
+    while(true){
+        getRat(&num1, &den1,&num2,&den2);
+        subRat(anum,aden, num1, den1,num2,den2);
+        char user = '\0';
+        cout << "Do you want to do more additions? (Y/N): ";
+        cin >> user;
+        cin.ignore();
+        if(user = 'N')
+        {
+            break;
+            quit();
+        }
+        else
+        {
+            subtraction();
+        }
+        
+    }
 }
